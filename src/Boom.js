9f7c13352f573292f9890eec6f1a12733f4bc9ac
Venky <venkateshdonthula2002@gmail.com>
2021-09-27 14:00:45 +0530

    update

--- src/Boom.js
+++ src/Boom.js
@@ -7,7 +7,7 @@ import { useSpeechSynthesis } from 'react-speech-kit'
 
 
 const Boom = (props) => {
-    var a = "Hii Welcome to Hey Dear"
+    var a = "Hey Welcome to Hey Dear"
     const { speak } = useSpeechSynthesis();
 
     return (
